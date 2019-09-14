6a8bed6167608a253c54e7230c0192681d1a8f3a
bimManager <kirillkozlov@ymail.com>
2019-09-14 14:12:18 +0100

    added google-c-style and set it up

--- init.el
+++ init.el
@@ -1,2 +1,6 @@
 (global-set-key (kbd "C-h") 'delete-backward-char)
-(global-set-key (kbd "C-w") 'backward-kill-word)		
+(global-set-key (kbd "C-w") 'backward-kill-word)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(load "google-c-style")
+(add-hook 'c-mode-common-hook 'google-set-c-style)
