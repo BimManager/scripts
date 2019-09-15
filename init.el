03b16c5b16d92955c0025b469225ad5e31ae203c
bimManager <kirillkozlov@ymail.com>
2019-09-15 09:15:52 +0100

    added a skeleton for Google C++ Style

--- init.el
+++ init.el
@@ -4,3 +4,16 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (load "google-c-style")
 (add-hook 'c-mode-common-hook 'google-set-c-style)
+
+(eval-after-load 'autoinsert
+  '(define-auto-insert
+     '("\\.\\(CC?\\|cc\\|cxx\\|cpp\\|c++\\)\\'" . "Google C++ Style")
+     '("Google C++ Style"
+       "// Copyright 2019 Samolet LLC" \n
+       "// Author: kkozlov" \n
+       "// "
+       (file-name-nondirectory (buffer-file-name))
+       \n \n \n)))
+
+(add-hook 'find-file-hook 'auto-insert)
+
